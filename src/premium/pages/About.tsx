30914d0c37c10b2870e78b1a3ef9060e4149c280
emreconf <emre.conf@gmail.com>
2026-02-09 19:48:07 +0300

    chore: bump version to 2.1.27 and sync all configs

--- src/premium/pages/About.tsx
+++ src/premium/pages/About.tsx
@@ -68,7 +68,7 @@ export default function About({ showToast }: AboutProps) {
               {t('premium')}
             </div>
           </div>
-          <div className="card-title">{t('version')} 2.1.26</div>
+          <div className="card-title">{t('version')} 2.1.27</div>
           <div className="card-description">
             {t('about_premium_desc')}
           </div>
